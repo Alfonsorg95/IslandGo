1da8acf9e44e62a3ead6b76f39e1eca95747f18d
Alfonso Rodriguez <alfonsorg95@gmail.com>
2023-05-05 17:16:53 +0200

    Home view showing map and stores when requested

--- IslandGoApp/home/static/home/modules/map.js
+++ IslandGoApp/home/static/home/modules/map.js
@@ -1,8 +1,10 @@
+import TOKEN from './config.js';
+
 import {
     updateSelectedStore,
 } from './stores.js';
 
-mapboxgl.accessToken = 'your-secret-key';
+mapboxgl.accessToken = TOKEN;
 
 /**
  * @typedef {import('./api').Store} Store
@@ -28,9 +30,17 @@ mapboxgl.accessToken = 'your-secret-key';
  * @return {Object} Map
  */
 export function addMap() {
-    const map = {}
-
+    const map = new mapboxgl.Map({  
+        container: 'map',  
+        style: 'mapbox://styles/mapbox/light-v10',  
+        center: [14.375416, 35.937496],  
+        zoom: 10 
+    });
+    
+    map.addControl(new mapboxgl.NavigationControl());  
+    
     return map;
+    
 }
 
 /**
@@ -39,7 +49,10 @@ export function addMap() {
  * @param {function} geocoderCallback - The callback that handles the response.
  */
 export function addGeocoder(map, geocoderCallback) {
-    const geocoder = {}
+    const geocoder = new MapboxGeocoder({ accessToken: mapboxgl.accessToken, mapboxgl: mapboxgl });map.addControl(geocoder);geocoder.on("result", (data) => {  
+        geocoderCallback(data);  
+    });
+    
 }
 
 /**
@@ -47,17 +60,48 @@ export function addGeocoder(map, geocoderCallback) {
  * @param {Store[]} stores
  * @return {StoresGeoJSON} Stores in GeoJSON
  */
-export function convertToGeoJson(stores) {
-    return {}
-}
+export function convertToGeoJson(stores) {return {  
+    type: "FeatureCollection",  
+    features: stores.map(store => {  
+        return {  
+            type: "Feature",  
+            geometry: {  
+                type: 'Point',  
+                coordinates: [store.longitude, store.latitude]  
+            },  
+            properties: {  
+                id: store.id,  
+                name: store.name,  
+                address: store.address,  
+                phone: store.phone,  
+                distance: store.distance,  
+                rating: store.rating,  
+            }  
+        }  
+    })  
+}}
 
 /**
  * Display stores on map
  * @param {Object} map
  * @param {StoresGeoJSON} storesGeoJson
  */
-export function plotStoresOnMap(map, storesGeoJson) {
-    
+export function plotStoresOnMap(map, storesGeoJson) {  
+    for(let store of storesGeoJson.features) {  
+        // create a HTML element for each feature  
+        let el = document.createElement('div');  
+        el.className = 'store';  
+        el.title = `${store.properties.name}\n` +  
+        `approximately ${store.properties.distance.toFixed(2)} km away\n` +  
+        `Address: ${store.properties.address || "N/A"}\n` +  
+        `Phone: ${store.properties.phone || "N/A"}\n` +  
+        `Rating: ${store.properties.rating || "N/A"}`; // make a marker for each feature and add to the map  
+        new mapboxgl.Marker(el)  
+            .setLngLat(store.geometry.coordinates)  
+            .addTo(map); el.addEventListener('click', function(e) {  
+            updateSelectedStore(store.properties.id);  
+        });
+    }  
 }
 
 /**
